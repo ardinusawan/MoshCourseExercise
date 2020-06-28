b197819c3d09f476459e73c3c11363f8dda08abe
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2020-06-28 15:14:17 +0800

    create addLast function

--- src/com/codewithmosh/LinkedList/LinkedList.java
+++ src/com/codewithmosh/LinkedList/LinkedList.java
@@ -1,11 +1,33 @@
 package com.codewithmosh.LinkedList;
 
 public class LinkedList {
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
     private Node first;
     private Node last;
 
     //addFirst
+
     //addLast
+    public void addLast(int item) {
+        var node = new Node(item);
+
+        if (first == null){
+            first = last = node;
+        }
+        else {
+            last.next = node;
+            last = node;
+        }
+    }
+
     //deleteFirst
     //deleteLast
     //contains
--- src/com/codewithmosh/LinkedList/Main.java
+++ src/com/codewithmosh/LinkedList/Main.java
@@ -2,6 +2,9 @@ package com.codewithmosh.LinkedList;
 
 public class Main {
     public static void main(String[] args){
-
+        var linkedList = new LinkedList();
+        linkedList.addLast(10);
+        linkedList.addLast(20);
+        linkedList.addLast(30);
     }
 }
